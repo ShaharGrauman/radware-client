0bf285d5d6b2c4e218b49585469a6763b4e5f51b
Rawan <rawan.1995.massarwi@gmail.com>
2020-01-29 20:03:01 +0200

    pull master

---
+++ client/src/components/admin/Register.js
@@ -0,0 +1,189 @@
+import React from 'react';
+import Table from './RolesDashboard';
+import RolesTable from './RolesTable';
+import Style from './Style.css';
+import SelectRole from './SelectRole2';
+import MyTable from '../shared/MyTable';
+export default class Register extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state={
+            form:{ firstname: '', lastname: '', phone:'', email:'', password:'', passwordconfirm:''},
+            errors:{}
+        };
+    }
+        
+    rolesData=[    
+            { rolename:"Role 1" , selected:<input type="checkbox" name="myTextEditBox" value="checked" />},
+            { rolename:"Role 1" , selected:<input type="checkbox" name="myTextEditBox" value="checked" />},
+            { rolename:"Role 1" , selected:<input type="checkbox" name="myTextEditBox" value="checked" />}
+        ];
+        tableHeaders = ["Roles", "Select"];
+
+    
+
+    firstNameError(input){
+        if (input.name === 'firstname') {
+            if(input.value.trim()==='') return 'first name is required';
+        } 
+    }
+
+    lastNameError(input){
+        if (input.name === 'lastname') {
+            if(input.value.trim()==='') return 'last name is required';
+        } 
+    }
+
+    phoneError(input){
+        if (input.name === 'phone') {
+            if(input.value.trim()==='') return 'phone is required';
+        } 
+    }
+    emailError(input) {
+        if (input.name === 'email') {
+          if (input.value.trim() === '') return 'email is required';
+        }
+        if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(input.value)) {
+          return "Email should be a valid email";
+        }
+      }
+    passwordError(input){
+        if (input.name === 'password') {
+            if(input.value.trim()==='') return 'password is required';
+        } 
+    }
+    confirmPasswordError(input){
+        if (input.name === 'confirmpassword') {
+            if(input.value.trim()==='') return 'password is required';
+        } 
+    }
+
+
+    validateProperty = (input) =>{
+        switch(input.name){
+            case 'firstname': return this.firstNameError(input);
+            case'lastname' : return this.lastNameError(input);
+            case 'phone': return this.phoneError(input);
+            case'email' : return this.emailError(input);
+            case 'password': return this.passwordError(input);
+            case'confirmpassword' : return this.confirmPasswordError(input);
+      
+        }
+        }
+          handleChange = ({currentTarget : input }) => {
+            const errors={...this.state.errors};
+            const errorMessage = this.validateProperty(input);
+             if (errorMessage)  errors[input.name]=errorMessage;
+             else delete errors[input.name];
+          
+            const form = { ...this.state.form };
+            form[input.name] = input.value;
+            this.setState({ form,errors });
+          
+          };
+          
+           validate = () =>{// validate form only 
+          
+            const errors={};
+           if (this.state.form.firstname.trim()==='' )
+            errors.firstname='first name is required ';
+            if (this.state.form.lastname.trim()==='' )
+            errors.lastname='last name is required ';
+            if (this.state.form.phone.trim()==='' )
+            errors.phone='phone is required ';
+            if (this.state.form.email.trim()==='' )
+            errors.email='email is required ';
+            if (this.state.form.password.trim()==='' )
+            errors.password='password is required ';
+            if (this.state.form.passwordconfirm.trim()==='' )
+            errors.passwordconfirm='confirm password is required ';
+            return Object.keys(errors).length===0 ? null : errors;
+           };
+              
+
+           
+ onSubmit = e => {
+    e.preventDefault();
+   const errors = this.validate();//method return object looks like error 
+    console.log(errors);
+    this.setState({ errors: errors || {} });//we render the object errors  in the setstate 
+   
+    if (errors) return;
+    
+    console.log("submitted");
+    console.log(this.state);
+  };
+
+    render() {
+        return (
+            <>
+                <form className="ml-3" onSubmit={this.onSubmit}>
+                    {/* <h3>Personal info</h3> */}
+                    <fieldset className="scheduler-border">
+                        <legend className="scheduler-border">Personal info</legend>
+
+                        {/* <h3>Personal info</h3> */}
+
+                        <div className="form-group mt-2 ml-2">
+                            <label htmlFor="firstname">First name : </label>
+                            <input className="form-control"  name= "firstname" type="text" id="fName" onChange={this.handleChange} />
+                            <div> { this.state.errors.firstname && <div className="alert alert-danger">{this.state.errors.firstname}</div>}</div>
+                            {/* <div class="valid-feedback">Valid.</div> */}
+
+                        </div>
+
+                        <div className="form-group ml-2">
+                            <label htmlFor="lName">Last name : </label>
+                            <input className="form-control" name="lastname" type="text" id="lName" onChange={this.handleChange} />
+                            <div> { this.state.errors.lastname && <div className="alert alert-danger">{this.state.errors.lastname}</div>}</div>
+
+                        </div>
+
+                        <div className="form-group ml-2">
+                            <label htmlFor="Rphone">Phone :</label>
+                            <input className="form-control" type="text" name= "phone" id="Rphone" onChange={this.handleChange} />
+                            <div> { this.state.errors.phone && <div className="alert alert-danger">{this.state.errors.phone}</div>}</div>
+
+                        </div>
+                        </fieldset>
+                        <fieldset className="scheduler-border">
+                        <legend className="scheduler-border">User info</legend>
+                        <div className="form-group ml-2">
+                            <label htmlFor="Remail">Email address :</label>
+                            <input type="email" className="form-control" name="email" id="Remail" aria-describedby="emailHelp" placeholder="name@example.com" onChange={this.handleChange} />
+                            <div> { this.state.errors.email && <div className="alert alert-danger">{this.state.errors.email}</div>}</div>
+                            <small className="form-text text-muted">This will be used as username.</small>
+                        </div>
+
+                        <div className="form-group ml-2">
+                            <label htmlFor="PassReg">Password :</label>
+                            <input type="password" className="form-control" name="password" id="PassReg" onChange={this.handleChange} />
+                            <div> { this.state.errors.password && <div className="alert alert-danger">{this.state.errors.password}</div>}</div>
+                            <small id="passwordHelp" className="form-text text-muted">Must be 6 at least 6 letters.</small>
+                        </div>
+                        {/* <div className="form-group ml-2">
+                            <label htmlFor="password"> Password Confirm : </label>
+                            <input required type="password" name="confirmpassword" className="form-control inputpass" minLength="4" maxLength="16" placeholder="Enter again to validate" id="password"/> */}
+                            {/* <div> { this.state.errors.passwordconfirm && <div className="alert alert-danger">{this.state.errors.passwordconfirm}</div>}</div> */}
+
+                        {/* </div> */}
+                        <p className="ml-2">Select role :</p>
+                        {/* <div className="col-4"></div>
+                        <div className= "col-6"> */}
+                            {/* <RolesTable /> */}
+                            {/* <SelectRole/> */}
+                            <SelectRole/>
+                            {/* <TableTest header={this.tableHeaders} data={this.rolesData}/> */}
+                        {/* </div> */}
+                        </fieldset>
+
+                        <button type="submit" className="btn btn-secondary btn-block" >Save</button>
+                        <button type="button" className="btn btn-secondary  btn-block">Cancel</button>
+                   
+                </form>
+            </>
+        );
+    }
+}
+
