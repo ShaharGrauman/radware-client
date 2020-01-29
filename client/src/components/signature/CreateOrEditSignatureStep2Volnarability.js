a0ea72dcadbef4468cfbb3a8b9c0e753b223811d
RanyHY <rany.gplus@gmail.com>
2020-01-29 21:18:14 +0200

    Create or Edit signature connect to server

--- client/src/components/signature/CreateOrEditSignatureStep2Volnarability.js
+++ client/src/components/signature/CreateOrEditSignatureStep2Volnarability.js
@@ -15,11 +15,6 @@ export default class CreateOrEditSignatureStep2Volnarability extends React.Compo
                 actions: [<Actions />]
             },
             {
-                vulnText: 'Giacomo Guilizzoni',
-                order: '^v',
-                actions: [<Actions />]
-            },
-            {
                 vulnText: 'Marco Botton',
                 order: '^v',
                 actions: [<Actions />]
@@ -38,22 +33,18 @@ export default class CreateOrEditSignatureStep2Volnarability extends React.Compo
             {
                 ParamName: 'Marco Botton',
                 actions: [<Actions />]
-            },
-            {
-                ParamName: 'Mariah Maclachlan',
-                actions: [<Actions />]
             }
         ];
     }
 
     render() {
         return (
-            <div>
+            <>
                 <div className="row ml-4">
-                    <h3>Define vulnerability by:</h3>
+                    <h5>Define vulnerability by:</h5>
                 </div>
                 <div className="row ml-3 mt-4">
-                    <div className="col-md-4">
+                    <div className="col-md-6">
                         <div>
                             <input type="radio" name="radio1" checked /> Simple text:
                         </div>
@@ -63,99 +54,105 @@ export default class CreateOrEditSignatureStep2Volnarability extends React.Compo
                             <label className="form-check-label" for="defaultCheck1">Regular expression</label>
                         </div>
                         <div className="mt-3">
-                            <label>Test Text : </label>
-                            <textarea type="text" className="form-control" id="testText" placeholder="Add text"></textarea>
+                            <label>Test text: </label>
+                            <textarea rows="7" type="text" className="form-control" id="testText" placeholder="Add text"></textarea>
                         </div>
                         <div className="mt-2 mb-2">
                             <button className="btn btn-block btn-secondary">Test</button>
                         </div>
                     </div>
-                    <div className="col-1"></div>
-                    <div className="col-md-5">
+                    <div className="col-md-6">
                         <input type="radio" name="radio2" /> Extended text
                         <div className="custom-control custom-switch float-right">
-                            <input type="checkbox" className="custom-control-input" id="toggle"></input>
-                            <label className="custom-control-label" for="toggle">Keep Order</label>
+                            <input name="keep_order" checked={this.props.signatureData.keep_order} onChange={this.props.onChangeHandler} type="checkbox" className="custom-control-input" id="toggle"></input>
+                            <label className="custom-control-label" for="toggle">Keep order</label>
                         </div>
                         <div>
-                            <input type="text" className="mt-2 form-control" placeholder="Add text"></input>
-                            <button className="btn btn-block btn-secondary mt-2 mb-3">Add</button>
+                            <div className="input-group sm-3 mb-2" >
+                                <input type="text" className="form-control" name="Add text" placeholder="Add text"></input>
+                                <div className="input-group-append">
+                                    <button type="button" className="btn btn-secondary">Add text</button>
+                                </div>
+                            </div>
                         </div>
                         <Table headers={this.dataHeader} data={this.data} />
                     </div>
                 </div>
                 <hr />
                 <div className="row ml-3">
-                    <div className="col-md-4">
-                        <h3>Scan at : </h3>
-                        <div className="col">
-                            <div className="form-check form-check-inline">
+                    <div className="col-md-6">
+                        <h6>Scan at : </h6>
+                        <div className="row container">
+                            <div className="form-check form-check-inline col">
                                 <input name="scanAtScanURl" checked={this.props.signatureData.scanAtScanURl} onChange={this.props.onChangeHandler} className="form-check-input" type="checkbox" />
                                 <label className="form-check-label" for="inlineCheckbox1">Scan URl</label>
                             </div>
-                            <div className="form-check form-check-inline">
+                            <div className="form-check form-check-inline col">
                                 <input name="scanAtScanParameters" checked={this.props.signatureData.scanAtScanParameters} onChange={this.props.onChangeHandler} className="form-check-input" type="checkbox" />
                                 <label className="form-check-label" for="inlineCheckbox2">Scan Parameters</label>
                             </div>
-                            <div className="form-check form-check-inline">
+                        </div>
+                        <div className="row container">
+                            <div className="form-check form-check-inline col">
                                 <input name="scanAtScanBody" checked={this.props.signatureData.scanAtScanBody} onChange={this.props.onChangeHandler} className="form-check-input" type="checkbox" />
                                 <label className="form-check-label" for="inlineCheckbox3">Scan Body</label>
                             </div>
-                            <div className="form-check form-check-inline">
+                            <div className="form-check form-check-inline col">
                                 <input name="scanAtScanFilename" checked={this.props.signatureData.scanAtScanFilename} onChange={this.props.onChangeHandler} className="form-check-input" type="checkbox" />
                                 <label className="form-check-label" onChange={this.props.onChangeHandler} for="inlineCheckbox4">Scan Filename</label>
                             </div>
+                        </div>
+                        <div className="row container">
                             <div className="form-check form-check-inline">
                                 <input name="scanAtScanHeaders" checked={this.props.signatureData.scanAtScanHeaders} onChange={this.props.onChangeHandler} className="form-check-input" type="checkbox" />
                                 <label className="form-check-label" for="inlineCheckbox5">Scan Headers</label>
                             </div>
                         </div>
                     </div>
-                    <div className="col-1"></div>
-                    <div className="col-md-5">
-                        <input type="text" className="mt-2 form-control" placeholder="Add Parameters"></input>
-                        <button className="btn btn-block btn-secondary mt-2 mb-3">Add Parameters</button>
+                    <div className="col-md-6">
+                    <div className="input-group sm-3 mb-2" >
+                                <input type="text" className="form-control" name="add parameters" placeholder="Add parametrs"></input>
+                                <div className="input-group-append">
+                                    <button type="button" className="btn btn-secondary">Add parameters</button>
+                                </div>
+                            </div>
                         <Table headers={this.paramHeader} data={this.param} />
                     </div>
                 </div>
-                <hr />
-                <div className="row ml-3">
-                    <div className="col-md-12">
-                        <div className="ml-4 mt-3">
-                            <div className="checkbox ml-1">
-                                <label>
-                                    <input type="checkbox" name="setStartBreak" checked={this.props.signatureData.setStartBreak} onChange={this.props.onChangeHandler}></input> Set Start Break
+                <div>
+                    <hr />
+                    <div className="checkbox">
+                        <label>
+                            <input type="checkbox" name="setStartBreak" checked={this.props.signatureData.setStartBreak} onChange={this.props.onChangeHandler}></input> Set Start Break
                                 </label>
-                                <div className="radio ml-4">
-                                    <label className="form-check form-check-inline">
-                                        <input type="radio" name="rbSetStartBreak" value="setStartBreakByFileName" checked={this.props.signatureData.rbSetStartBreak === 'setStartBreakByFileName'} onChange={this.props.onChangeHandler} disabled={!this.props.signatureData.setStartBreak}></input>
-                                        <small className="ml-1">By FILENAME (?)</small>
-                                    </label>
-                                    <label className="form-check form-check-inline">
-                                        <input type="radio" name="rbSetStartBreak" value="setStartBreakByAlpha" checked={this.props.signatureData.rbSetStartBreak === 'setStartBreakByAlpha'} onChange={this.props.onChangeHandler} disabled={!this.props.signatureData.setStartBreak}></input>
-                                        <small className="ml-1">By Alpha (?)</small>
-                                    </label>
-                                </div>
-                            </div>
-                            <div className="checkbox ml-1">
-                                <label>
-                                    <input type="checkbox" name="setEndBreak" checked={this.props.signatureData.setEndBreak} onChange={this.props.onChangeHandler}></input> Set End Break
+                        <div className="radio ml-4">
+                            <label className="form-check form-check-inline">
+                                <input type="radio" name="start_break" value="setStartBreakByFileName" checked={this.props.signatureData.start_break === 'setStartBreakByFileName'} onChange={this.props.onChangeHandler} disabled={!this.props.signatureData.setStartBreak}></input>
+                                <label className="ml-1">By FILENAME (?)</label>
+                            </label>
+                            <label className="form-check form-check-inline">
+                                <input type="radio" name="start_break" value="setStartBreakByAlpha" checked={this.props.signatureData.start_break === 'setStartBreakByAlpha'} onChange={this.props.onChangeHandler} disabled={!this.props.signatureData.setStartBreak}></input>
+                                <label className="ml-1">By Alpha (?)</label>
+                            </label>
+                        </div>
+                    </div>
+                    <div className="checkbox">
+                        <label>
+                            <input type="checkbox" name="setEndBreak" checked={this.props.signatureData.setEndBreak} onChange={this.props.onChangeHandler}></input> Set End Break
                                 </label>
-                                <div className="radio ml-4">
-                                    <label className="form-check form-check-inline">
-                                        <input type="radio" name="rbSetEndBreak" value="setEndBreakByFileName" checked={this.props.signatureData.rbSetEndBreak === 'setEndBreakByFileName'} onChange={this.props.onChangeHandler} disabled={!this.props.signatureData.setEndBreak}></input>
-                                        <small className="ml-1">By FILENAME (?)</small>
-                                    </label>
-                                    <label className="form-check form-check-inline">
-                                        <input type="radio" name="rbSetEndBreak" value="setEndBreakByAlpha" checked={this.props.signatureData.rbSetEndBreak === 'setEndBreakByAlpha'} onChange={this.props.onChangeHandler} disabled={!this.props.signatureData.setEndBreak}></input>
-                                        <small className="ml-1">By Alpha (?)</small>
-                                    </label>
-                                </div>
-                            </div>
+                        <div className="radio ml-4">
+                            <label className="form-check form-check-inline">
+                                <input type="radio" name="end_break" value="setEndBreakByFileName" checked={this.props.signatureData.end_break === 'setEndBreakByFileName'} onChange={this.props.onChangeHandler} disabled={!this.props.signatureData.setEndBreak}></input>
+                                <label className="ml-1">By FILENAME (?)</label>
+                            </label>
+                            <label className="form-check form-check-inline">
+                                <input type="radio" name="end_break" value="setEndBreakByAlpha" checked={this.props.signatureData.end_break === 'setEndBreakByAlpha'} onChange={this.props.onChangeHandler} disabled={!this.props.signatureData.setEndBreak}></input>
+                                <label className="ml-1">By Alpha (?)</label>
+                            </label>
                         </div>
                     </div>
                 </div>
-            </div>
+            </>
         );
     }
 }
