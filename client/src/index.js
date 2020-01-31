75dda5e59c9003a20446c9e04abea9ade0fdf52f
Rawan <rawan.1995.massarwi@gmail.com>
2020-01-31 15:15:38 +0200

    fixing post request admin and role

--- client/src/index.js
+++ client/src/index.js
@@ -11,9 +11,9 @@ import NewRole from './components/admin/NewRole';
 import Audit from './components/admin/Audit';
 import RolesDashboard from './components/admin/RolesDashboard';
 import LogIn from './components/admin/LogIn';
-import CreateOrEditSignatureWizard from './components/signature/CreateOrEditSignatureWizard';
-import SearchSignature from './components/reports/SearchSignature/SearchSignature';
-import ControlSteps from './components/shared/ControlSteps';
+// import CreateOrEditSignatureWizard from './components/signature/CreateOrEditSignatureWizard';
+// import SearchSignature from './components/reports/SearchSignature/SearchSignature';
+// import ControlSteps from '../components/shared/ControlSteps';
 
 class App extends React.Component {
     render() {
@@ -26,7 +26,7 @@ class App extends React.Component {
                     <Route path="/newuser">
                         <NewUserDashbaord />
                     </Route>
-                    <Route path="/roleslist">
+                    <Route path="/admin/roles">
                         <RolesDashboard />
                     </Route>
 
@@ -36,14 +36,16 @@ class App extends React.Component {
                     <Route path="/login">
                         <LogIn />
                     </Route>
-                      <Route path="/SearchSignature">
-                        <SearchSignature />
-                       <Audit />
+                    <Route path="/audit">
+                        <Audit />
                     </Route>
-                    <Route path="/">
+                    <Route path="/users">
                         <AdminDashbaord />
                     </Route>
-                 
+                    {/* <Route path="/SearchSignature">
+                        <SearchSignature />
+                    </Route> 
+                   */}
                 </Switch>
             </BrowserRouter>
              );
