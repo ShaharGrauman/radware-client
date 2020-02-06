04cfdae7d18a676bb1bc850da2fb9b7540124b30
mohammeddall <mm_dl@live.com>
2020-02-06 22:49:49 +0200

    QA dashboardQA dashboard git status q !

--- client/src/components/reports/QADashboard/QADashboard.js
+++ client/src/components/reports/QADashboard/QADashboard.js
@@ -4,6 +4,16 @@ import axios from 'axios'
 import {Dropdown, ButtonGroup, Button} from 'react-bootstrap';
 import {Container,Col,Row} from 'react-bootstrap';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowRight,
+  faArrowLeft
+} from "@fortawesome/free-solid-svg-icons";
+
 
 import QATable from './QATable';
 import './QADashboard.css';
@@ -14,97 +24,102 @@ export default class QADashboard extends React.Component {
     constructor(props) {
         super(props);
         this.state={
-            data : [
-                // {
-                //     seq_id:1, 
-                //     patternID: "AAA", 
-                //     URI: 1,
-                //     headers: 1,
-                //     body:1, 
-                //     parameters: 1, 
-                //     file: 0,
-                //     manual: "passed",
-                //     performance: "init", 
-                //     automation: 'init'
-                // },
-    
-                // {
-                //     seq_id:2, 
-                //     patternID: "BBB", 
-                //     URI: 0,
-                //     headers: 1,
-                //     body:1,  
-                //     parameters: 1, 
-                //     file: 1, 
-                //     manual: "init",
-                //     performance: "failed", 
-                //     automation: 'passed'
-                // },
-    
-                // {
-                //     seq_id:3, 
-                //     patternID: "CCC", 
-                //     URI: 1,
-                //     headers: 0, 
-                //     body:1, 
-                //     parameters: 0, 
-                //     file: 1, 
-                //     manual: "init",
-                //     performance: "init", 
-                //     automation: 'failed'
-                // },
-                // {
-                //     seq_id:4, 
-                //     patternID: "DDD", 
-                //     URI: 0,
-                //     headers: 0, 
-                //     body:0, 
-                //     parameters: 0, 
-                //     file: 1, 
-                //     manual: "init",
-                //     performance: "passed", 
-                //     automation: 'init'
-                // },
-                // {
-                //     seq_id:5, 
-                //     patternID: "DDD", 
-                //     URI: 0,
-                //     headers: 1, 
-                //     body:0, 
-                //     parameters: 1, 
-                //     file: 0, 
-                //     manual: "failed",
-                //     performance: "failed", 
-                //     automation: 'init'
-                // },
-                // {
-                //     seq_id:6, 
-                //     patternID: "DDD", 
-                //     URI: 0,
-                //     headers: 1, 
-                //     body:0, 
-                //     parameters: 1, 
-                //     file: 0, 
-                //     manual: "init",
-                //     performance: "init", 
-                //     automation: 'failed'
-                // },
-                // {
-                //     seq_id:7, 
-                //     patternID: "DDD", 
-                //     URI: 0,
-                //     headers: 1, 
-                //     body:0, 
-                //     parameters: 1, 
-                //     file: 0, 
-                //     manual: "failed",
-                //     performance: "failed", 
-                //     automation: 'failed'
-                // }
-            ]
+            updateSelectedButton:'Update selected',
+            hasNext:'true',
+            hasPrev:'false',
+            page:1,
+            dataToShow:[]
 
         };
-        
+        this.data = [
+            // {
+            //     seq_id:1, 
+            //     patternID: "AAA", 
+            //     URI: 1,
+            //     headers: 1,
+            //     body:1, 
+            //     parameters: 1, 
+            //     file: 0,
+            //     manual: "passed",
+            //     performance: "init", 
+            //     automation: 'init'
+            // },
+
+            // {
+            //     seq_id:2, 
+            //     patternID: "BBB", 
+            //     URI: 0,
+            //     headers: 1,
+            //     body:1,  
+            //     parameters: 1, 
+            //     file: 1, 
+            //     manual: "init",
+            //     performance: "failed", 
+            //     automation: 'passed'
+            // },
+
+            // {
+            //     seq_id:3, 
+            //     patternID: "CCC", 
+            //     URI: 1,
+            //     headers: 0, 
+            //     body:1, 
+            //     parameters: 0, 
+            //     file: 1, 
+            //     manual: "init",
+            //     performance: "init", 
+            //     automation: 'failed'
+            // },
+            // {
+            //     seq_id:4, 
+            //     patternID: "DDD", 
+            //     URI: 0,
+            //     headers: 0, 
+            //     body:0, 
+            //     parameters: 0, 
+            //     file: 1, 
+            //     manual: "init",
+            //     performance: "passed", 
+            //     automation: 'init'
+            // },
+            // {
+            //     seq_id:5, 
+            //     patternID: "DDD", 
+            //     URI: 0,
+            //     headers: 1, 
+            //     body:0, 
+            //     parameters: 1, 
+            //     file: 0, 
+            //     manual: "failed",
+            //     performance: "failed", 
+            //     automation: 'init'
+            // },
+            // {
+            //     seq_id:6, 
+            //     patternID: "DDD", 
+            //     URI: 0,
+            //     headers: 1, 
+            //     body:0, 
+            //     parameters: 1, 
+            //     file: 0, 
+            //     manual: "init",
+            //     performance: "init", 
+            //     automation: 'failed'
+            // },
+            // {
+            //     seq_id:7, 
+            //     patternID: "DDD", 
+            //     URI: 0,
+            //     headers: 1, 
+            //     body:0, 
+            //     parameters: 1, 
+            //     file: 0, 
+            //     manual: "failed",
+            //     performance: "failed", 
+            //     automation: 'failed'
+            // }
+        ];
+        this.size=5;
         this.role=['manual','performance','automation']       
         // this.role=['manual','performance','automation']       
     }
@@ -112,7 +127,9 @@ export default class QADashboard extends React.Component {
     async componentDidMount() {
         try{
             const {data} = await axios.get('http://localhost:3000/Qa/dashboard');
-            this.setState({data:data})
+            this.data=data;
+            console.log(this.data)
+            this.setState({dataToShow:data.slice(0,this.size),page:1})
             // console.log(this.state.data)
         }catch(error){
               window.alert('Error');       
@@ -121,8 +138,7 @@ export default class QADashboard extends React.Component {
 
     updateData = async e=>{
         const urlBody=[];
-        if(window.confirm('are you sure you want to update signatures!')){
-        this.state.data.forEach(signature=>{
+        this.data.forEach(signature=>{
             const oneSigUpdate={id:signature.id}
             this.role.forEach(role=>{
                 oneSigUpdate[role]=signature[role]
@@ -132,17 +148,19 @@ export default class QADashboard extends React.Component {
         })
         
         try{
-            console.log(JSON.stringify(urlBody));
-          await axios.put('http://localhost:3000/Qa/dashboard',JSON.stringify(urlBody),{headers: {"Content-Type": "application/json"}});
+            // console.log(JSON.stringify(urlBody));
+          const {data:msg}=await axios.put('http://localhost:3000/Qa/dashboard',JSON.stringify(urlBody),{headers: {"Content-Type": "application/json"}});
+          this.setState({updateSelectedButton:msg})
+          //   console.log(msg,this.state.msg)
         }catch(error){
+            this.setState({updateSelectedButton:'Error in update'})
             window.alert('Error');       
         }
         // console.log(urlBody)
-    }
     } 
 
     render() {
-        if(this.state.data.length==0){
+        if(this.data.length==0){
             return (
                 <>
                 <h2 className="ml-2 mb-3">QA dashboard</h2>
@@ -176,15 +194,65 @@ export default class QADashboard extends React.Component {
                 <div className="container ml-0 mt-2">
                     <div className="row">
                         <div className="col">
-                            <QATable data={this.state.data}  role={this.role}/>
+                        <QATable data={this.state.dataToShow}  role={this.role}/>
                         </div>
                     </div>
+                    <div className="row mb-3">
+                      <div className="col-1 col-sm-1 col-md-2 col-lg-3 mx-sm-1 mx-md-2 mx-lg-0"></div>
+                      <div className="col-3 col-sm-3 col-md-2 ml-5 " >
+                        {this.state.page>1?
+                          <span className="fas" className="noselect ml-5"  onClick={()=>{
+                            const newPage=this.state.page-1
+                            this.setState({dataToShow:this.data.slice((newPage-1)*this.size,newPage*this.size)})
+                            this.setState({page:newPage});
+                        
+                          }}>
+                          <FontAwesomeIcon
+                            icon={faArrowLeft}
+                          ></FontAwesomeIcon>{" "}
+                          Previous
+                        </span>
+                        : null  
+                      }
+        
+                      </div>
+                      <div className="col-1 col-lg-0 mx-2 mx-sm-2 mx-md-0">
+                    <span class="badge badge-secondary">{this.state.page}</span>
+                    
+                      </div>
+                      <div className="col-3 col-sm-2">
+                      {this.size*this.state.page<this.data.length?
+                        <span className="fas" onClick={()=>{
+                          const newPage=this.state.page+1
+                          this.setState({dataToShow:this.data.slice((newPage-1)*this.size,newPage*this.size)})
+                        
+                          this.setState({page:newPage});
+                        }}>
+                          Next{" "}
+                          <FontAwesomeIcon
+                            icon={faArrowRight}
+                          ></FontAwesomeIcon>
+                        </span>          
+                    :null
+                    }
+
+              </div>
+            </div>
                 </div>
-                <Container>
                     <Row>
                         <Col xs={2}></Col>
                         <Col xs={2}>
-                        <button className="btn btn-secondary " type="submit" width="50px" onClick={this.updateData}>Update selected</button>
+                        <button className="btn btn-secondary " type="submit" width="50px" onClick={()=>{
+                            if(window.confirm('are you sure you want to update signatures!')){
+                                this.setState({updateSelectedButton:'loading...'});                    
+                                this.updateData();
+                                setTimeout(()=>
+                                    this.setState({updateSelectedButton:'Update Selecte'})
+                                , 3000);
+
+                            }}}>
+                            {this.state.updateSelectedButton}
+                        </button>
                         
                         </Col>
                         <Col xs={2}>
@@ -192,7 +260,7 @@ export default class QADashboard extends React.Component {
                         <Dropdown as={ButtonGroup}>
                     <Button variant="secondary " onClick={()=>{
                         if (window.confirm(`Are you sure you want to check all signature status to passed`)){                               
-                            this.state.data.forEach(signature => {
+                            this.data.forEach(signature => {
                                 this.role.forEach(role=>
                                     signature[role]='passed'
                                     )        
@@ -204,7 +272,7 @@ export default class QADashboard extends React.Component {
                       {this.role.map((role,index)=>
                           <Dropdown.Item  onClick={()=>{
                             if (window.confirm(`Are you sure you want to check all signature status in ${role} passed`)){                               
-                            this.state.data.forEach(signature => {
+                            this.data.forEach(signature => {
                                 signature[role]='passed'
                             });
                             this.setState({})
@@ -218,7 +286,7 @@ export default class QADashboard extends React.Component {
                         <Dropdown as={ButtonGroup}>
                             <Button variant="secondary" onClick={()=>{
                                      if (window.confirm(`Are you sure you want to check all signature status to failed`)){                               
-                                    this.state.data.forEach(signature => {
+                                    this.data.forEach(signature => {
                                         this.role.forEach(role=>
                                             signature[role]='failed'
                                             )        
@@ -230,7 +298,7 @@ export default class QADashboard extends React.Component {
                               {this.role.map((role,index)=>
                                   <Dropdown.Item  onClick={()=>{
                                     if (window.confirm(`Are you sure you want to check all signature status in ${role} failed`)){
-                                    this.state.data.forEach(signature => {
+                                    this.data.forEach(signature => {
                                         signature[role]='failed'
                                     });
                                     this.setState({})
@@ -238,10 +306,8 @@ export default class QADashboard extends React.Component {
                               )}
                               </Dropdown.Menu>
                         </Dropdown>
-
                         </Col>
                     </Row>
-                </Container>                
             </>);      
     }
     }
--- client/src/components/reports/QADashboard/QaStatuseUpdate.js
+++ client/src/components/reports/QADashboard/QaStatuseUpdate.js
@@ -11,34 +11,34 @@ export default class QaStatuseUpdate extends React.Component {
     render() {
         return (<>
         <div className="form-check form-check-inline">
-                        <input className="form-check-input" type="radio" name={`${this.state.name}${this.state.role}`} 
-                            id={`${this.state.name}${this.state.role}1`} value="init" checked={this.state.signature[this.state.role]==='init'} 
+                        <input className="form-check-input" type="radio" name={`${this.props.signature.id}${this.state.role}`} 
+                            id={`${this.props.signature.id}${this.state.role}1`} value="init" checked={this.props.signature[this.state.role]==='init'} 
                             onClick={()=>{
-                                this.state.signature[this.state.role]='init';
+                                this.props.signature[this.state.role]='init';
                                 this.setState({});                                
                             }}
                         ></input>
-                        <label className="form-check-label" htmlFor={`${this.state.name}${this.state.role}1`}>Init</label>
+                        <label className="form-check-label" htmlFor={`${this.props.signature.id}${this.state.role}1`}>Init</label>
                     </div>
                     <div className="form-check form-check-inline">
-                        <input className="form-check-input" type="radio" name={`${this.state.name}${this.state.role}`}
-                            id={`${this.state.name}${this.state.role}2`} value="passed"  checked={this.state.signature[this.state.role]==='passed'}
+                        <input className="form-check-input" type="radio" name={`${this.props.signature.id}${this.state.role}`}
+                            id={`${this.props.signature.id}${this.state.role}2`} value="passed"  checked={this.props.signature[this.state.role]==='passed'}
                             onClick={()=>{
-                                this.state.signature[this.state.role]='passed';
+                                this.props.signature[this.state.role]='passed';
                                 this.setState({});                                
                             }}
                             ></input>
-                        <label className="form-check-label" htmlFor={`${this.state.name}${this.state.role}2`}>Passed</label>
+                        <label className="form-check-label" htmlFor={`${this.props.signature.id}${this.state.role}2`}>Passed</label>
                     </div>
                     <div className="form-check form-check-inline">
-                        <input className="form-check-input" type="radio" name={`${this.state.name}${this.state.role}`}
-                            id={`${this.state.name}${this.state.role}3`} value="failed" checked={this.state.signature[this.state.role]==='failed'}
+                        <input className="form-check-input" type="radio" name={`${this.props.signature.id}${this.state.role}`}
+                            id={`${this.props.signature.id}${this.state.role}3`} value="failed" checked={this.props.signature[this.state.role]==='failed'}
                             onClick={()=>{
-                                this.state.signature[this.state.role]='failed';
+                                this.props.signature[this.state.role]='failed';
                                 this.setState({});                                
                             }}
                             ></input>
-                        <label className="form-check-label" htmlFor={`${this.state.name}${this.state.role}3`}>Failed</label>
+                        <label className="form-check-label" htmlFor={`${this.props.signature.id}${this.state.role}3`}>Failed</label>
                     </div>
         </>);
     }
