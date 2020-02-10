24b140d9410ad86d83528e4fda2649f8dc8a759f
shireen <shireen.fahmawe@gmail.com>
2020-02-10 20:51:02 +0200

    Export Dropdown

--- client/src/components/reports/SearchSignature/ResearcherDashboard.js
+++ client/src/components/reports/SearchSignature/ResearcherDashboard.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 import axios from 'axios';
-
+import { Dropdown } from 'react-bootstrap';
+import { Redirect } from 'react-router-dom';
 
 import { Link } from 'react-router-dom';
 
@@ -36,7 +37,8 @@ export default class ResearcherDashboard extends React.Component {
       all:true,
       currentButton:'all',
       clickedButton:'',
-      errorMsg:''
+      errorMsg:'',
+      searchClicked: false
     }
     this.urlDetails={
       page: 1 ,
@@ -44,20 +46,44 @@ export default class ResearcherDashboard extends React.Component {
     };
   }
 
+  renderRedirect = page => {
+    if (page === "search") {
+        this.setState({
+            searchClicked: true
+        });
+    }
+}
+
   componentDidMount() {
-    this.loadData('All');
+      try{
+        axios.get(`http://localhost:3001/signature/researcher`).then(res=>{
+          this.setState({hasNext:res.hasNext,hasPrev:res.hasPrev})
+          console.log(res.data.signatureData);
+          if(res.data.signatureData.length == 0){
+            this.setState({data: [
+              { patternID: '', description: '' }
+             
+            ]});         
+          }else{
+            this.setState({data:res.data.signatureData});
+          }
+          
+          })
+        }catch(error){
+                this.setState({
+                  errorMsg: 'Error'
+                });
+            }
   }
 
   loadData(filter){
     let requestURL;
     this.setState({currentButton:filter});
+    console.log("current:"+this.state.currentButton);
     if(this.state.currentButton == filter){//to return to all when double clicking
       this.setState({currentButton:"all"}); // to set currentButton to all when clicking twice at button
       requestURL=`http://localhost:3001/signature/researcher`;
       this.setState({dataFilter:"All Signatures"}); 
-    }else if (this.state.currentButton == 'all' ){
-      this.setState({dataFilter:"All Signatures"}); 
-      requestURL=`http://localhost:3001/signature/researcher`;
     }else{
       requestURL=`http://localhost:3001/signature/researcher?status=${filter}`;
       Object.keys(this.urlDetails).forEach(key=>requestURL=requestURL.concat(`&${key}=${this.urlDetails[key]}`))
@@ -99,6 +125,8 @@ export default class ResearcherDashboard extends React.Component {
     this.urlDetails.page =1;
     this.setState({clickedButton:value});
     this.setState({dataFilter:`${value} Signatures`});
+    console.log("clicked:"+this.state.clickedButton);
+    
     // console.log(currentButton,value)
     // let current = this.state.currentButton;
     // if (current === value) {
@@ -115,34 +143,65 @@ export default class ResearcherDashboard extends React.Component {
 
     return (
       <>
+        {this.state.searchClicked && <Redirect to='/SearchSignature' />}
         <h2 className="ml-2 mb-3">Researcher dashboard</h2>
-        <div>
+        <div className='row'>
+        <div className='ml-2 mr-4'>
           <button type="button" className="ml-2 mr-4 btn btn-secondary">
             New
           </button>
-          <button
+          
+          {/* <div class="dropdown">
+          <button class="dropbtn" onclick="myFunction()">Export
+            <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-content" id="myDropdown">
+            <a href="#">QA</a>
+            <a href="#">Testing</a>
+            <a href="#">Git</a>
+          </div>
+          </div>  */}
+          {/* <button
             className="btn btn-secondary dropdown-toggle mr-4"
             type="button"
             id="dropdownMenuButton"
             data-toggle="dropdown"
             aria-haspopup="true"
-            aria-expanded="false"
+            aria-expanded="true"
+            // onClick={this.myFunction()}
           >
             Export
-          </button>
-          <Link to="/SearchSignature">Search</Link>
-          <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a className="dropdown-item" href="#">
-              1
-            </a>
-            <a className="dropdown-item" href="#">
-              2
-            </a>
-            <a className="dropdown-item" href="#">
-              3
-            </a>
-          </div>
+          </button> */}
+          <button type="button"
+                            onClick={() => this.renderRedirect("search")}
+                            className="ml-2 mr-4 btn btn-secondary">Search</button>
 
+         </div>
+
+          {/* <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a className="dropdown-item" href="#">
+              QA
+            </a>
+            <a className="dropdown-item" href="#">
+              Testing
+            </a>
+            <a className="dropdown-item" href="#">
+              Git
+            </a>
+          </div> */}
+          <div className='ml-2 mr-4'> 
+          <Dropdown >
+          <Dropdown.Toggle className="btn btn-secondary dropdown-toggle mr-4" id="dropdown-basic">
+            Export
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">QA</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Testing</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Git</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        </div>
         </div>
         <div className="ml-2 mt-3 mx-">{this.state.dataFilter} by Create Date</div>
         <div className="container ml-0">
