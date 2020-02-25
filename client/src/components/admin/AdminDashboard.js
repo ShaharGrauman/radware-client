0477bf448a89756ba56e63d5ec587e8daa69ba6e
Amin <aminshbi@gmail.com>
2020-02-25 19:58:12 +0200

    adding button for Deleted Users

--- client/src/components/admin/AdminDashboard.js
+++ client/src/components/admin/AdminDashboard.js
@@ -14,19 +14,18 @@ class AdminDashboard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      orgUsers: [],
+      activeUsers: [],
       users: [],
+      deletedUsers: [],
+      sortedDeletedUsers: [],
       id: 0,
       newUserClicked: false,
       deleteUserClicked: false,
+      showDeletedUsersClicked: false,
       modalIsOpen: false,
       deleteUser:'',
       ifUserDeleted:false,
     }
-  }
-
-  showModal = () => {
-
   }
 
   closeModal = () => {
@@ -62,28 +61,49 @@ class AdminDashboard extends React.Component {
   SortByKey(sortKey) {
     let sorted;
     if (sortKey.sortOrder)
-      sorted = this.state.orgUsers.sort((a, b) => a[sortKey.key] > b[sortKey.key] ? 1 : -1);
+      sorted = this.state.users.sort((a, b) => a[sortKey.key] > b[sortKey.key] ? 1 : -1);
 
     else
-      sorted = this.state.orgUsers.sort((a, b) => a[sortKey.key] < b[sortKey.key] ? 1 : -1);
+      sorted = this.state.users.sort((a, b) => a[sortKey.key] < b[sortKey.key] ? 1 : -1);
     sortKey.sortOrder = !sortKey.sortOrder;
     this.setState({ users: sorted })
   }
-
+  showDeletedUsers = async e => {
+    await this.setState({showDeletedUsersClicked: !this.state.showDeletedUsersClicked})
+    if(this.state.showDeletedUsersClicked){
+      this.setState({users: this.state.deletedUsers})
+    } 
+    else{
+      this.setState({users: this.state.activeUsers})
+    } 
+  }
   async componentDidMount() {
     const users = await getUsers();
-    const usersWithRoles = users.map(user => ({
-      ...user,
-      roles: user.roles.map(role => role.name).join(', '),
-      actions: [
-        <button type="button" key={user.id + "edit"} title="Edit" className="btn btn-outline float-left "><Link to={`/edit_user/${user.id}`} ><FontAwesomeIcon className="fa-lg " icon={faEdit}> </FontAwesomeIcon></Link></button>,
-        <button type="button" key={user.id + "delete"} title="Delete" className="btn  btn-outline float-left" onClick={() => this.ifClickedDeleteUser(user.username)}><FontAwesomeIcon className="fa-lg " icon={faTrash}></FontAwesomeIcon></button>,
-      ]
-    }));
-
+    const usersWithRoles = [];
+    const deletedUsers = [];
+    users.forEach(user => {
+      if(user.status != "deleted"){
+        usersWithRoles.push({...user, roles: user.roles.map(role => role.name).join(', '),
+        actions: [
+          <button type="button" key={user.id + "edit"} title="Edit" className="btn btn-outline float-left "><Link to={`/edit_user/${user.id}`} ><FontAwesomeIcon className="fa-lg " icon={faEdit}> </FontAwesomeIcon></Link></button>,
+          <button type="button" key={user.id + "delete"} title="Delete" className="btn  btn-outline float-left" onClick={() => this.ifClickedDeleteUser(user.username)}><FontAwesomeIcon className="fa-lg " icon={faTrash}></FontAwesomeIcon></button>,
+        ]})
+      }
+      else{
+        deletedUsers.push({...user, roles: user.roles.map(role => role.name).join(', '),
+        actions: [
+          <button type="button" key={user.id + "edit"} title="Edit" className="btn btn-outline float-left ">
+            <Link to={`/edit_user/${user.id}`} ><FontAwesomeIcon className="fa-lg " icon={faEdit}> </FontAwesomeIcon>
+              </Link>
+              </button>    
+        ]
+      })
+      }
+    })
     this.setState({
-      orgUsers: usersWithRoles,
-      users: usersWithRoles
+      activeUsers: usersWithRoles,
+      users: usersWithRoles,
+      deletedUsers: deletedUsers
     });
   }
 
@@ -91,12 +111,11 @@ class AdminDashboard extends React.Component {
   { key: "name", value: "Name", toSort: true, sortOrder: true },
   { key: "username", value: "Username", toSort: true, sortOrder: true },
   { key: "phone", value: "Phone", toSort: false, sortOrder: true },
-  { key: "status", value: "Status", toSort: true, sortOrder: true },
+  { key: "status", value: "Status", toSort: false, sortOrder: true },
   { key: "roles", value: "Roles", toSort: true, sortOrder: true },
   { key: "actions", value: "", toSort: false, sortOrder: true }
 
   ];
-
   render() {
 
     if(this.state.ifUserDeleted){
@@ -116,36 +135,43 @@ class AdminDashboard extends React.Component {
  
     return (
       <>
-        <div>
           {this.state.newUserClicked && <Redirect to='/newuser' />}
-          <div className="row">
-            <div className="col-md-11 col-sm-11 col-11">
-              <div className="ml-3 mb-3">
+          <div className="row ml-3 mr-3">
+            <div className="col-lg-10 col-md-9 col-sm-6">
                 <h2>Admin Dashboard</h2>
                 <h4>Users` Management</h4>
-              </div>
             </div>
-
-            <div className=" ml-3 mb-4">
-              <button type="button" title="add user" className="btn btn-outline float-left" onClick={() => this.renderRedirect("newuser")}>
+            <div className="col ml-3 mb-3 float-right">
+              <div>
+              <button type="button" title="add user" className="btn btn-outline mb-3 float-right" onClick={() => this.renderRedirect("newuser")}>
                 <FontAwesomeIcon size="3x" icon={faUserPlus}></FontAwesomeIcon></button>
+                </div>
             </div>
           </div>
-          <div className="row">
-            <div className=" mr-3 mb-4"></div>
+          <div className="row ml-3 mr-3">
+<div className="col-10"></div>
+<div className="col-2">
+<button type="button" title="Show Deleted Users" className="btn btn-primary mb-3 float-right"style={{ width: "200px" }}
+            onClick={this.showDeletedUsers}>
+            {(!this.state.showDeletedUsersClicked && "Show Deleted Users") ||
+            (this.state.showDeletedUsersClicked && "Back to Users")}
+                </button>
+</div>
+                
           </div>
 
-          <div className=" ml-3 mr-3">
-            <div className="row">
+          <div className="row ml-3 mr-3">
               <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12" >
                 <AdminTable
                   header={this.tableHeaders}
                   data={this.state.users}
+                  
                   sortDataByKey={(sortKey) => this.SortByKey(sortKey)}
                   className="col-lg-12 col-md-12 col-sm-12 col-xs-12" ></AdminTable>
               </div>
-            </div></div>
-        </div>
+            </div>
+
+        
       </>
 
     );
