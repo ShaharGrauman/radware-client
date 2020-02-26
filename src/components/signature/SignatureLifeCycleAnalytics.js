63b41d199c5fae566bd21bfb9df32bdb2f255185
Ibraheem Ganayim <Brhoma.king@gmail.com>
2020-02-26 20:51:11 +0200

    Analysis

--- src/components/signature/SignatureLifeCycleAnalytics.js
+++ src/components/signature/SignatureLifeCycleAnalytics.js
@@ -4,9 +4,9 @@ import BarChart from './AnalysisChart';
 export default class SignatureLifeCycleAnalytics extends Component {
 	render() {
 		return (
-			<div>				
-				<p style={{textAlign:"center", margin:"50px",color:"grey"}}>Signature <b>created</b> at 02/02/2020 18:08:45</p>
-				<BarChart />
+			<div>
+				<p style={{ textAlign: "center", margin: "50px", color: "grey" }}>Signature <b>created</b> at {`${this.props.signatureData.creation_date.replace(/-/g, '/')} ${this.props.signatureData.creation_time}`} {}</p>
+				<BarChart signatureData={this.props.signatureData}/>
 				<p style={{textAlign:"center", margin:"50px",color:"grey"}}>Signature <b>published</b> at 06/02/2020 14:00:00</p>
 			</div>
 		)
